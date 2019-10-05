a32c046193d9250e75d0ce0ebe6828c666127faf
Ilya Siganov <ilya.blan4@gmail.com>
2019-10-05 15:32:03 +0600

    fix gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ group = "org.catinthedark"
 version = "1.0-SNAPSHOT"
 
 tasks.wrapper {
-    val gradleVersion = "5.6.2"
+    val gradleVersion = "5.4.1"
     distributionUrl = "https://services.gradle.org/distributions/gradle-$gradleVersion-all.zip"
+}
+
+plugins {
+    kotlin("jvm") version "1.3.50" apply false
 }
--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     application
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm")
     id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
